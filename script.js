c87434d9d6f7e290e426581215cea269ec030387
kiehp <kierra.nelson@aol.com>
2021-07-16 17:08:32 -0400

    bootstrap linked/js updates

--- script.js
+++ script.js
@@ -1,6 +1,90 @@
 console.log("attached");
 
-alert("Are you ready to test your javascript knowledge?")
+window.alert("Are you ready to test your javascript knowledge?");
+
+var startButton = document.getElementById('start-btn')
+var nextButton = document.getElementById('next-btn')
+var questionContainerElement = document.getElementById('question-container')
+var questionElement = document.getElementById('question')
+var answerButtonsElement = document.getElementById('answer-options')
+
+//start the quiz with score of 0
+var quizScore = 0;
+//loop over question object array
+for (var i = 0; i < questions.length; i++) {
+//display current question to user, ask ok or cancel
+    var answer = confirm(  questions[i].q  );
+    if (
+        (answer === true && questions[i].a === 't') ||
+        (answer === false && questions[i].a === 'f')
+    ){
+//increase score
+        score++;
+
+function startGame() {
+  startButton.classList.add('hide')
+}
+
+//questions + answers object array
+const questions = [ 
+  {
+  question: 'Inside which HTML element do we put the js',
+  answers: [
+    { text: 'script tag', correct: true },
+    { text: 'scripting', correct: false },
+    { text: 'js', correct: false }
+  ]
+},
+
+  {
+  question: 'What is a var in terms of javascript?',
+  answers: [
+    { text: 'a name assigned to a literal value or object', correct: true },
+    { text: 'the quality or state of having different types', correct: false },
+    { text: 'value-added resource', correct: false }
+  ]
+},
+
+{
+question: 'read as js statement and solve: consolelog(2.0=="2"== newBoolean(true)=="1")',
+answers: [ 
+  { text: 'true', correct: true },
+  { text: 'false', correct: false },
+  { text: '-1', correct: false }
+]
+},
+
+{
+  question: 'HTML, Javascript, or CSS?',
+  answers: [
+    { text: 'Javascript', correct: true },
+    { text: 'HTML', correct: false },
+    { text: 'CSS', correct: false }
+  ]
+},
+
+{
+  question: 'Explain the keyword this.',
+  answers: [
+    { text: 'refers to the object that the function is a property of', correct: true },
+    { text: 'null', correct: false },
+    { text: 'this', correct: false }
+  ]
+}
+]
+//at the end of the quiz, show the results /total 
+        var quizEnd = function() {
+          if (quizScore < 4 || quizScore === 4){
+      //show total at end of quiz
+      alert('you got' + quizScore + '/5' + questions.length);
+          }
+          else if (quizScore === 5) {
+              alert("your score is" + quizScore + '/5' + questions.length + '.' + "Not bad, you aced the test!");
+          }
+      };
+      quizEnd();
+
+
 
 //var timerEl = document.getElementById('countdown');
 //var mainEl = document.getElementById('main');
@@ -37,40 +121,8 @@ alert("Are you ready to test your javascript knowledge?")
 //startBtn.onclick = countdown;
 
 //question object array
-var questions = [
-    { Q1: 'Inside which HTML element do we put the js?', a: 'script'},
-    { Q2: 'What is a var in terms of javascript?', a: 'a name assigned to a literal value or object'},
-    { Q3: 'read as js statement and solve: consolelog(2.0=="2"== newBoolean(true)=="1")', a: 'true'},
-    { Q4: 'Explain the keyword this.', a: 'refers to the object that the function is a property of.'},
-    { Q5: 'HTML, Javascript, or CSS?', a: 'Javascript'},
-];
-
-//start the js quiz with score of 0
-var quizScore = 0;
-//loop over question object array
-for (var i = 0; i < questions.length; i++) {
-//display current question to user, ask ok or cancel
-    var answer = confirm(  questions[i].q  );
-    if (
-        (answer === true && questions[i].a === 't') ||
-        (answer === false && questions[i].a === 'f')
-    ){
-//increase score
-        score++;
-//alert user if question is right or wrong
-  //      alert('correct');
-  //  } else {
-   //     alert('wrong');
-  //  }
-//};
-
-var quizEnd = function() {
-    if (quizScore < 4 || quizScore === 4){
-//show total at end of quiz
-alert('you got' + quizScore + '/5' + questions.length);
-    }
-    else if (quizScore === 5) {
-        alert("your score is" + quizScore + '/5' + questions.length + '.' + "Not bad, you aced the test!");
-    }
-};
-quizEnd();
+    //{ Q1: 'Inside which HTML element do we put the js?', a: 'script'},
+    //{ Q2: 'What is a var in terms of javascript?', a: 'a name assigned to a literal value or object'},
+    //{ Q3: 'read as js statement and solve: consolelog(2.0=="2"== newBoolean(true)=="1")', a: 'true'},
+    //{ Q4: 'Explain the keyword this.', a: 'refers to the object that the function is a property of.'},
+    //{ Q5: 'HTML, Javascript, or CSS?', a: 'Javascript'}
