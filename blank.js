a6c0a86f6449a5c3bf092323cba24530327d68fa
kiehp <kierra.nelson@aol.com>
2021-07-31 17:05:19 -0400

    dynamic functionality

---
+++ blank.js
@@ -0,0 +1,17 @@
+var cat= {
+    name: "jj",
+    age: 1,
+    adopted: true,
+    sound: function(){
+        console.log("meow")
+    }
+}
+var dog= {
+    name: "jj",
+    age: 1,
+    adopted: true,
+    sound: function(){
+        console.log("meow")
+    }
+}
+dog.sound();
--- script.js
+++ script.js
@@ -4,6 +4,7 @@ window.alert("Are you ready to test your javascript knowledge?");
 
 var startButton = document.getElementById('start-btn')
 var nextButton = document.getElementById('next-btn')
+var submitQuiz = document.getElementById('results')
 var questionContainerElement = document.getElementById('question-container')
 var questionElement = document.getElementById('question')
 var answerButtonsElement = document.getElementById('answer-options')
@@ -11,19 +12,10 @@ var buttonEl = document.getElementById('')
 var answerButtons = document.getElementsByClassName('btn')
 var questionTxtContainer = document.getElementById('question-txt')
 var questionNumber = 0
+var timerDivEl = document.getElementById('timer')
+//start game with a score of 0 
+var score= 0;
 
-for (var i = 0; i < answerButtons.length; i++) {
-  answerButtons[i].addEventListener('click', onclick)
-}
-startButton.addEventListener('click', startGame)
-function onclick(event){
-  console.log(event.target.value);
-  questionNumber++;
-  changeQuestion();
-}
-function changeQuestion(){
-  questionTxtContainer.textContent= questions[questionNumber].question
-}
 //questions + answers object array
 const questions = [
   {
@@ -71,35 +63,57 @@ const questions = [
     ]
   },
 ]
-
-//start the quiz with score of 0
-var quizScore = 0;
-//loop over question object array
-// for (var i = 0; i < questions.length; i++) {
-//   //display current question to user, ask ok or cancel
-//   var answer = confirm(questions[i].q);
-//   if (
-//     (answer === true && questions[i].a === 't') ||
-//     (answer === false && questions[i].a === 'f')
-//   ) {
-//     //increase score
-//     score++;
-//   }
-// }
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', nextQuestion)
+resultsButton.addEventListener('click', endGame)
+//start quiz
 function startGame() {
+  //create timer
+  var count = 60, timer = setInterval(function() {
+    timerDivEl.innerHTML = count--;
+    if(count == 1) clearInterval(timer);
+}, 1000);
+
   startButton.setAttribute('class', 'hide')
   questionTxtContainer.textContent= questions[0].question
 }
+//loop over every question object 
+for (var i = 0; i < questions.length; i++) {
+  //display current question to user and ask ok/cancel
+  var answer = confirm(questions[i].q);
+
+  //compare answers
+  if ((answer === true && questions[i].a === "t") ||
+  (answer === false && questions[i].a === "f")) {
+    //increase score
+    score++;
+    alert("correct!");
+  }
+  else {
+    alert("wrong!");
+  }
+}
+//show total at end
+alert("you got" + score + "/" + questions.length);
+
+//on click, listen for user answer
+for (var i = 0; i < answerButtons.length; i++) {
+  answerButtons[i].addEventListener('click', nextQuestion)
+}
+//change question
+function changeQuestion(){
+  questionTxtContainer.textContent= questions[questionNumber].question
+}
+//next question
+function nextQuestion(event) {
+  console.log(event.target.value);
+  questionNumber++;
+  changeQuestion();
+}
 
 //at the end of the quiz, show the results /total 
-var quizEnd = function () {
-  if (quizScore < 4 || quizScore === 4) {
-    //show total at end of quiz
-    alert('you got' + quizScore + '/5' + questions.length);
-  }
-  else if (quizScore === 5) {
-    alert("your score is" + quizScore + '/5' + questions.length + '.' + "Not bad, you aced the test!");
-  }
-};
-//quizEnd();
+function endGame() {
+  console.log(resultsButton);
+}
+
 //buttonEl.addEventListener('click', quizBoxHandler);
