50030d4669ffeae0619d34cc7d77f6c4e42aeb67
kiehp <kierra.nelson@aol.com>
2021-10-23 00:04:20 -0400

    updated files

--- logic.js
+++ logic.js
@@ -1,1 +1,48 @@
-
+//questions + answers object array
+const questions = [
+    {
+      question: 'Inside which HTML element do we put the js',
+      answers: [
+        { text: 'script tag', correct: true },
+        { text: 'scripting', correct: false },
+        { text: 'js', correct: false }
+      ]
+    },
+  
+    {
+      question: 'What is a var in terms of javascript?',
+      answers: [
+        { text: 'a name assigned to a literal value or object', correct: true },
+        { text: 'the quality or state of having different types', correct: false },
+        { text: 'value-added resource', correct: false }
+      ]
+    },
+  
+    {
+      question: 'read as js statement and solve: consolelog(2.0=="2"== newBoolean(true)=="1")',
+      answers: [
+        { text: 'true', correct: true },
+        { text: 'false', correct: false },
+        { text: '-1', correct: false }
+      ]
+    },
+  
+    {
+      question: 'HTML, Javascript, or CSS?',
+      answers: [
+        { text: 'Javascript', correct: true },
+        { text: 'HTML', correct: false },
+        { text: 'CSS', correct: false }
+      ]
+    },
+  
+    {
+      question: 'Explain the keyword this.',
+      answers: [
+        { text: 'refers to the object that the function is a property of', correct: true },
+        { text: 'null', correct: false },
+        { text: 'this', correct: false }
+      ]
+    },
+  ]
+  
--- script.js
+++ script.js
@@ -7,66 +7,21 @@ var questionContainerElement = document.getElementById('question-container')
 var questionElement = document.getElementById('question')
 var answerButtonsElement = document.getElementById('answer-options')
 var buttonEl = document.getElementById('')
-var answerButtons = document.getElementsByClassName('btn')
+var answerButtons = document.getElementsByClassName('btn-grid')
 var resultsButton = document.getElementById('submit')
 var questionTxtContainer = document.getElementById('question-txt')
 var questionNumber = 0
 var timerDivEl = document.getElementById('timer')
+var nextQuestion = document.getElementById('question-txt')
 //start game with a score of 0 
 var score= 0;
-
-//questions + answers object array
-const questions = [
-  {
-    question: 'Inside which HTML element do we put the js',
-    answers: [
-      { text: 'script tag', correct: true },
-      { text: 'scripting', correct: false },
-      { text: 'js', correct: false }
-    ]
-  },
-
-  {
-    question: 'What is a var in terms of javascript?',
-    answers: [
-      { text: 'a name assigned to a literal value or object', correct: true },
-      { text: 'the quality or state of having different types', correct: false },
-      { text: 'value-added resource', correct: false }
-    ]
-  },
-
-  {
-    question: 'read as js statement and solve: consolelog(2.0=="2"== newBoolean(true)=="1")',
-    answers: [
-      { text: 'true', correct: true },
-      { text: 'false', correct: false },
-      { text: '-1', correct: false }
-    ]
-  },
-
-  {
-    question: 'HTML, Javascript, or CSS?',
-    answers: [
-      { text: 'Javascript', correct: true },
-      { text: 'HTML', correct: false },
-      { text: 'CSS', correct: false }
-    ]
-  },
-
-  {
-    question: 'Explain the keyword this.',
-    answers: [
-      { text: 'refers to the object that the function is a property of', correct: true },
-      { text: 'null', correct: false },
-      { text: 'this', correct: false }
-    ]
-  },
-]
 
 //event listeners for start, next and submit (results) button 
 startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', nextQuestion)
 resultsButton.addEventListener('click', endGame)
+answerButtons.addEventListener('click', startGame);
+
 
 //start quiz
 function startGame() {
@@ -83,6 +38,7 @@ function startGame() {
 for (var i = 0; i < questions.length; i++) {
   //display current question to user and ask ok/cancel
   var answer = confirm(questions[i].q);
+console.log(answer);
 
   //compare answers
   if ((answer === true && questions[i].a === "t") ||
@@ -102,9 +58,13 @@ alert("you got" + score + "/" + questions.length);
 for (var i = 0; i < answerButtons.length; i++) {
   answerButtons[i].addEventListener('click', nextQuestion)
 }
+
+
+
+
 //change question
 function changeQuestion(){
-  questionTxtContainer.textContent= questions[questionNumber].question
+  questionTxtContainer.textContent= questions[i].question
 }
 //next question
 function nextQuestion(event) {
@@ -121,4 +81,5 @@ function endGame() {
 
 
 
-//buttonEl.addEventListener('click', quizBoxHandler);
+
+
